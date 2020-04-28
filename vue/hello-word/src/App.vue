068c544478fabfccfbf57b374b8c56acb40ee7e2
兰军 <lanjun@encdata.cn>
2020-04-28 09:53:46 +0800

    vue mint

--- vue/hello-word/src/App.vue
+++ vue/hello-word/src/App.vue
@@ -2,16 +2,26 @@
   <div>
     <img class="logo" src="./assets/logo.png" alt="logo">
     <HelloWorld/>
+    <mt-button @click="handleClick" size="small" type="danger" style="position: absolute;
+     left: 50%;
+     transform: translate(-50%, -50%);">
+      Test
+    </mt-button>
   </div>
 </template>
 
 <script>
+    import {Toast} from 'mint-ui'
     import HelloWorld from "./components/HelloWorld";
 
     export default {
-        components: {HelloWorld}
+        components: {HelloWorld},
+        methods: {
+            handleClick() {
+                Toast(' 点击了测试');
+            }
+        }
     }
-
 </script>
 
 
--- vue/hello-word/src/main.js
+++ vue/hello-word/src/main.js
@@ -1,5 +1,8 @@
 import Vue from "vue"
 import App from "./App";
+import {Button} from "mint-ui"
+
+Vue.component(Button.name,Button)
 
 new Vue({
     el: '#app',
